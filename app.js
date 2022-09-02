e8575bba0a0c8e309ae797a3450215e2ad388e69
Nikoloz Shervashidze <nikoshervashidze@gmail.com>
2022-09-02 19:59:26 +0400

    add localstorage & error message for validation

--- app.js
+++ app.js
@@ -49,8 +49,66 @@ dropdown.addEventListener('change', () => {
 const modal = document.getElementById('modal');
 const btn = document.getElementById('save-btn');
 
-btn.onclick = function () {
-	modal.style.display = 'block';
-	document.body.style.overflow = 'hidden';
-	document.body.style.height = '100%';
+// btn.onclick = function () {
+// 	modal.style.display = 'block';
+// 	document.body.style.overflow = 'hidden';
+// 	document.body.style.height = '100%';
+// };
+
+// LocalStorage
+
+const firstName = document.getElementById('firstname');
+const surname = document.getElementById('surname');
+const team = document.getElementById('team');
+const position = document.getElementById('position');
+const email = document.getElementById('email');
+const telephone = document.getElementById('telephone');
+// const errorMsg = document.getElementById('error-msg');
+
+window.onbeforeunload = function () {
+	localStorage.setItem('firstName', firstName.value);
+	localStorage.setItem('surname', surname.value);
+	localStorage.setItem('team', team.value);
+	localStorage.setItem('teamName', team.name);
+	localStorage.setItem('position', position.value);
+	localStorage.setItem('email', email.value);
+	localStorage.setItem('telephone', telephone.value);
 };
+
+window.onload = function () {
+	if (!firstName.value) {
+		firstName.value = localStorage.firstName;
+	}
+	if (!surname.value) {
+		surname.value = localStorage.surname;
+	}
+	if (!team.value) {
+		team.value = localStorage.team;
+	}
+	if (!position.value) {
+		position.value = localStorage.position;
+	}
+	if (!email.value) {
+		email.value = localStorage.email;
+	}
+	if (!telephone.value) {
+		telephone.value = localStorage.telephone;
+	}
+};
+
+// FIRT PAGE SUBMIT BUTTON
+
+function error(name, message) {
+	name.classList.add('red-alert');
+	name.parentNode.style.color = 'red';
+	const parentElement = name.parentElement;
+	const error = parentElement.querySelectorAll('#error-msg');
+	error[0].innerText = message;
+}
+
+const nextBtn = document.getElementById('next-btn');
+
+nextBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	error(firstName, 'RADIOACTIVE ZONE');
+});
